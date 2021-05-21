0b9e341914c690e15bae1c52ddfa3cfa6ee8239e
Ricky Pai <rickyp999+github@gmail.com>
2021-05-21 07:53:20 -0700

    only need to process half

--- problems/125validpalindrome/125_valid_palindrome.go
+++ problems/125validpalindrome/125_valid_palindrome.go
@@ -17,7 +17,7 @@ func isPalindrome(s string) bool {
 }
 
 func isPalindromeStr(s string) bool {
-	for i := 0; i < len(s); i++ {
+	for i := 0; i < len(s)/2; i++ {
 		if s[i] != s[len(s)-i-1] {
 			return false
 		}
--- problems/9palindromenumber/9_palindrome_number.go
+++ problems/9palindromenumber/9_palindrome_number.go
@@ -11,7 +11,7 @@ func isPalindrome(x int) bool {
 }
 
 func isPalindromeStr(s string) bool {
-	for i := 0; i < len(s); i++ {
+	for i := 0; i < len(s)/2; i++ {
 		if s[i] != s[len(s)-i-1] {
 			return false
 		}
